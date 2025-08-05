4362ab326b1ef712770d8e6a2fec642c82505d45
olihager <ophager@gmail.com>
2025-08-04 22:33:23 -0300

    webhook video

--- src/controllers/whatsappcontrollers.js
+++ src/controllers/whatsappcontrollers.js
@@ -1,8 +1,25 @@
 console.log("✅ THIS IS THE CLEANED CONTROLLER");
 
 const verifyToken = (req, res) => {
-  console.log("✅ verifyToken was hit");
-  res.send("✅ FROM verifyToken in controller");
+  
+
+try {
+
+  var accessToken = "ringoringo1232";
+  var token = req.query["hub.verify_token"];
+  var challenge = req.body["hub.challenge"];
+
+  if (challenge != null && token != null && token == accessToken){
+    res.send(challenge);
+  }else {
+    res.status(400).send();
+  }
+  
+} catch (error) {
+  res.status(400).send();
+}
+
+  
 };
 
 const messageReceived = (req, res) => {
